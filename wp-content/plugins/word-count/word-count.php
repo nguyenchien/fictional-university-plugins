b98927017c30ee5aba4a50d05f84b17893e4eada
chienvn <chiennguyen1702@gmail.com>
2023-07-27 05:00:59 +0700

    loco translate plugin

--- wp-content/plugins/word-count/word-count.php
+++ wp-content/plugins/word-count/word-count.php
@@ -4,12 +4,19 @@
   Description: This is a test plugin
   Author: Chien Nguyen
   Version: 1.0
+  Text Domain: wcpdomain
+  Domain Path: /languages
 */
 class WordCount {
   function __construct() {
     add_action('admin_menu', array($this, 'adminPage'));
     add_action('admin_init', array($this, 'settings'));
     add_filter('the_content', array($this, 'ifWrap'));
+    add_action('init', array($this, 'languages'));
+  }
+  
+  function languages() {
+    load_plugin_textdomain('wcpdomain', false, dirname(plugin_basename(__FILE__)).'/languages');
   }
   
   function ifWrap($content) {
@@ -27,15 +34,15 @@ class WordCount {
     $wordCount = str_word_count(strip_tags($content));
     
     if (get_option('wcp_wordcount', '1')) {
-      $html .= 'The post has ' . $wordCount . ' words<br>';
+      $html .= __('The post has', 'wcpdomain') . ' ' . $wordCount . ' words<br>';
     }
     
     if (get_option('wcp_charactercount', '1')) {
-      $html .= 'The post has '. strlen(strip_tags($content)) . ' characters<br>';
+      $html .= __('The post has', 'wcpdomain') . ' ' . strlen(strip_tags($content)) . ' characters<br>';
     }
     
     if (get_option('wcp_readtime', '1')) {
-      $html .= 'The post will take about '. round($wordCount/225) . ' minute(s) to read';
+      $html .= __('The post will take about', 'wcpdomain') . ' ' . round($wordCount/225) . ' minute(s) to read';
     }
     
     $html .= "</p>";
@@ -50,23 +57,23 @@ class WordCount {
     add_settings_section( 'wcp_first_section', null, null, 'word-count-settings-page' );
 
     // Display Location
-    add_settings_field( 'wcp_location', 'Display Location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section');
+    add_settings_field( 'wcp_location', __('Display Location', 'wcpdomain'), array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section');
     register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback'=>array($this, 'sanitizeLocation'), 'default'=>'0'));
 
     // Headline Text
-    add_settings_field( 'wcp_headline', 'Headline Text', array($this, 'headlineHTML'), 'word-count-settings-page', 'wcp_first_section');
+    add_settings_field( 'wcp_headline', __('Headline Text', 'wcpdomain'), array($this, 'headlineHTML'), 'word-count-settings-page', 'wcp_first_section');
     register_setting('wordcountplugin', 'wcp_headline', array('sanitize_callback'=>'sanitize_text_field', 'default'=>'Headline Default'));
 
     // Word Count
-    add_settings_field( 'wcp_wordcount', 'Word Count', array($this, 'checkBoxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName'=>'wcp_wordcount'));
+    add_settings_field( 'wcp_wordcount', __('Word Count', 'wcpdomain'), array($this, 'checkBoxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName'=>'wcp_wordcount'));
     register_setting('wordcountplugin', 'wcp_wordcount', array('sanitize_callback'=>'sanitize_text_field', 'default'=>'1'));
     
     // Character Count
-    add_settings_field( 'wcp_charactercount', 'Character Count', array($this, 'checkBoxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName'=>'wcp_charactercount'));
+    add_settings_field( 'wcp_charactercount', __('Character Count', 'wcpdomain'), array($this, 'checkBoxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName'=>'wcp_charactercount'));
     register_setting('wordcountplugin', 'wcp_charactercount', array('sanitize_callback'=>'sanitize_text_field', 'default'=>'1'));
     
     // Read Time
-    add_settings_field( 'wcp_readtime', 'Read Time', array($this, 'checkBoxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName'=>'wcp_readtime'));
+    add_settings_field( 'wcp_readtime', __('Read Time', 'wcpdomain'), array($this, 'checkBoxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName'=>'wcp_readtime'));
     register_setting('wordcountplugin', 'wcp_readtime', array('sanitize_callback'=>'sanitize_text_field', 'default'=>'1'));
   }
 
