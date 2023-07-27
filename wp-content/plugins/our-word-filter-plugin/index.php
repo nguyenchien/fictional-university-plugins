12dc53bca299aa4654439352617e76f6315e19a1
chienvn <chiennguyen1702@gmail.com>
2023-07-28 06:46:58 +0700

    form

--- wp-content/plugins/our-word-filter-plugin/index.php
+++ wp-content/plugins/our-word-filter-plugin/index.php
@@ -19,22 +19,27 @@ class ourWordFilterPlugin {
   function mainPageAssess() {
     wp_enqueue_style('filterAdminCss', plugin_dir_url( __FILE__).'style.css');
   }
+  function handleSubmit() {
+    echo 'hello 123';
+  }
   function ourWordFilterHTML() { ?>
     <div class="wrap">
       <h1>Word Filter</h1>
-      <form method="post">
+      <?php if (isset($_POST['justsubmitted']) && $_POST['justsubmitted'] == "true") $this->handleSubmit(); ?>
+      <form method="POST">
+        <input type="hidden" name="justsubmitted" value="true">
         <label for="plugin_words_to_filter">
           <p>Enter a <strong>momma-separated</strong> list of words to filter from your site content.</p>
           <div class="word-filter__flex-container">
-            <textarea name="plugin_words_to_filter" id="plugin_words_to_filter" placeholder="bad, mean, abc, xyz, ..."></textarea>
+            <textarea name="plugin_words_to_filter" id="plugin_words_to_filter" placeholder="bad, mean, abc, xyz"></textarea>
           </div>
           <input type="submit" name="submit" class="button button-primary" value="Save Changes">
         </label>
       </form>
     </div>
-  <? }
+  <?php }
   function optionsSubPage() { ?>
     <p>hello options</p>
-  <? }
+  <?php }
 }
 $ourWordFilterPlugin = new ourWordFilterPlugin();
