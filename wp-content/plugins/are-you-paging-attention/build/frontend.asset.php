4885fad03341fe768189819eff8ea58cf99f9555
chienvn <chiennguyen1702@gmail.com>
2023-08-13 10:50:13 +0700

    user click answer

--- wp-content/plugins/are-you-paging-attention/build/frontend.asset.php
+++ wp-content/plugins/are-you-paging-attention/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'ca5ec6a89f6a0042bf3c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '0a31e9e9fcbe30b638ef');
