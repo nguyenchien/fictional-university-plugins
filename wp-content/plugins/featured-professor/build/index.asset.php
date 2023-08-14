b0312ed615e153ed0ccbd9d84a380040152be319
chienvn <chiennguyen1702@gmail.com>
2023-08-14 21:13:38 +0700

    setup starter plugin

---
+++ wp-content/plugins/featured-professor/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-element'), 'version' => '6a3710643025df94acf3');
--- wp-content/plugins/featured-professor/src/index.js
+++ wp-content/plugins/featured-professor/src/index.js
@@ -5,17 +5,25 @@ wp.blocks.registerBlockType("ourplugin/featured-professor", {
   description: "Include a short description and link to a professor of your choice",
   icon: "welcome-learn-more",
   category: "common",
+  attributes: {
+    professorID: {type: "string"}
+  },
   edit: EditComponent,
   save: function () {
     return null
   }
 })
 
-function EditComponent() {
+function EditComponent(props) {
   return (
     <div className="featured-professor-wrapper">
       <div className="professor-select-container">
-        We will have a select dropdown form element here.
+        <select onChange={(e)=>{props.setAttributes({professorID: e.target.value})}}>
+          <option value="">Select a option</option>
+          <option value="1" selected={props.attributes.professorID == 1}>professor 1</option>
+          <option value="2" selected={props.attributes.professorID == 2}>professor 2</option>
+          <option value="3" selected={props.attributes.professorID == 3}>professor 3</option>
+        </select>
       </div>
       <div>
         The HTML preview of the selected professor will appear here.
